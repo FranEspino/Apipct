1395d37c03cbe597d628465b1625a2740320a9d6
FranEspino <Frapo_Developer@outlook.com.pe>
2021-12-16 16:27:32 -0500

    Api in aws

---
+++ mysql/keys.ts
@@ -0,0 +1,9 @@
+module.exports = {
+    database: {
+        host: "ec2-18-119-12-103.us-east-2.compute.amazonaws.com",
+        user: "fraporitmos",
+        password: "fraporitmos*2021",
+        database: "pct"
+    }
+
+}
